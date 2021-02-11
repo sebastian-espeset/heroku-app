ad04416ea8b41b09203a08d7a204a21b0f708c4f
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-11 12:58:14 -0600

    added stuff to react app, is pulling from server directly

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
 import './App.css';
 
+//create a function that returns a path(url)
+//checks to see what the node env is(dev,staging, whatver), and builds a url path from that
+
+function url(path){
+  return process.env.NODE_ENV === "development"?
+  `http://localhost:3333${path}`: path
+};
+
 function App() {
+  const [ data, setData ]=useState("hello bubblegum, from app state ");
+  useEffect(()=>{
+    fetch(url("/api/"))
+      .then(res=>{
+        return res.json()
+      }).then(apiData=>{
+        setData(apiData.data)
+      }).catch(error=>{
+        console.log("busted, idk")
+      })
+  },[])
+ 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        API DATA: {data}
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path")
 
 const app = express();
-const port = process.env.port || 9000;
+const port = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"client/build","index.html")))
+
 
 app.use("/api/*",(_,res)=>{
-    res.json({data:`Hello bubblegum data`})
+    res.json({data:`Hello bubblegum, from data, `})
 })
 
 app.listen(port,()=>{
