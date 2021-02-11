8f57f4064cb2ce92ad47eef32be2389d6353eda9
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-11 12:15:53 -0600

    Will this work?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.Port || 9000
+const port = process.env.Port || 9000;
 
 app.use(cors());
 app.use(express.json());
